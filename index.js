18314501e8ea7619fb28245fb5255a273b21a5c0
Arjun <arj.kashyap123@gmail.com>
2020-04-09 18:53:29 +0530

    click listener for each state

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+
+/* 
+{
+    id,
+    connectedNodes[],
+    selected
+}
+*/
 let states = []
 
 $(document).ready(function(){
@@ -5,9 +13,20 @@ $(document).ready(function(){
     // Add Node on button click
     $('#add-state').click(function(){
         const nodeId = states.length;
-        $('.draw-area').append(`<div class='node'> <p> Q${nodeId} </p>  </div>`);
+        $('.draw-area').append(`<div id='q${nodeId}' class='node'> <p> Q${nodeId} </p>  </div>`);
         $(".node").draggable();
-        states.push({ id: nodeId, connectedNodes: [] });
+        states.push({ id: nodeId, connectedNodes: [], selected: false });
     });
+
+
+    // Functions if the node is selected
+    function nodeCheckClick(){
+         states.forEach( n =>{
+             //console.log(`node: q${n.id}`)
+             $(`#q${n.id}`).mousedown( () => console.log(`Clicked..q${n.id}`) );
+         });
+     }
+    
+   setInterval(nodeCheckClick, 200);
 })
 
