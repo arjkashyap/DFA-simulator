827375799861cbc83f7e87b1a5d6d031686c392d
Arjun <arj.kashyap123@gmail.com>
2020-04-09 20:01:48 +0530

    select events and remove state button functional

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 /* 
-{
+states{
     id,
     connectedNodes[],
     selected
@@ -10,6 +10,7 @@ let states = []
 
 $(document).ready(function(){
 
+    // Button functions
     // Add Node on button click
     $('#add-state').click(function(){
         const nodeId = states.length;
@@ -18,15 +19,45 @@ $(document).ready(function(){
         states.push({ id: nodeId, connectedNodes: [], selected: false });
     });
 
+    $('#del-state').click(function(){
+        states.forEach( n => {
+            if(n.selected){
+                $(`#q${n.id}`).remove();
+                states.splice(n.id, 1);
+            }
+        } )
+    })
 
     // Functions if the node is selected
     function nodeCheckClick(){
          states.forEach( n =>{
-             //console.log(`node: q${n.id}`)
-             $(`#q${n.id}`).mousedown( () => console.log(`Clicked..q${n.id}`) );
+             $(`#q${n.id}`).mousedown( () => {
+                n.selected = true; 
+                disSelectAll(n);
+               });
          });
      }
-    
-   setInterval(nodeCheckClick, 200);
+    setInterval(nodeCheckClick, 200);
+
+
+    // Diselect other nodes if one is selected
+    function disSelectAll(selectedNode = 0){
+        states.forEach( n => {
+            if(n.id != selectedNode.id)
+                n.selected = false;
+        })
+    }
+
+   // $('.draw-area').mousedown(() => )
+
+    // Check select state
+    setInterval(() => {
+        states.forEach( n => {
+            let color = '#577ee8';
+            if(n.selected)
+                color = '#abeada';
+            $(`#q${n.id}`).css('background-color', color);
+        } )
+    }, 200);
 })
 
